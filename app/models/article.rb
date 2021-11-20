fa1f76cfb30d680af41876534e5f97b14c2f2fc7
Aizaz Ahmad <aizazahamd90@gmail.com>
2021-11-20 15:17:37 +0500

    Deleting Associated Model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,8 @@ class Article < ApplicationRecord
     # Include Visible Concern
     include Visible
     #https://guides.rubyonrails.org/getting_started.html#associating-models
-    has_many :comments
+    # https://guides.rubyonrails.org/getting_started.html#deleting-associated-objects
+    has_many :comments, dependent: :destroy 
     #https://guides.rubyonrails.org/getting_started.html#validations-and-displaying-error-messages
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
