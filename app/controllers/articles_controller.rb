30232203498e924d6f39b413eda5865ca872e101
Aizaz Ahmad <aizazahamd90@gmail.com>
2021-11-20 12:22:09 +0500

    Articles CRUD Complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,41 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    # https://guides.rubyonrails.org/getting_started.html#creating-a-new-article
+    def new
+        @article = Article.new
+    end
+    def create
+        @article = Article.new(article_params)
+
+        if @article.save
+            redirect_to @article
+        else
+            render :new
+        end
+    end
+    # https://guides.rubyonrails.org/getting_started.html#updating-an-article
+    def edit
+        @article = Article.find(params[:id])
+    end
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render :edit
+        end
+    end
+    #https://guides.rubyonrails.org/getting_started.html#deleting-an-article
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to root_path
+    end
+    private
+        # https://guides.rubyonrails.org/getting_started.html#using-strong-parameters
+        def article_params
+            params.require(:article).permit(:title, :body)
+        end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    #https://guides.rubyonrails.org/getting_started.html#validations-and-displaying-error-messages
+    validates :title, presence: true
+    validates :body, presence: true, length: {minimum: 10}
 end
